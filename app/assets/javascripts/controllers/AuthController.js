2216dea99396bd034f79f9167be7ebb8df9429c2
CJ <thecjhorton@gmail.com>
2017-03-10 22:57:26 -0600

    Devise/Angular login/register is mostly working

--- app/assets/javascripts/controllers/AuthController.js
+++ app/assets/javascripts/controllers/AuthController.js
@@ -4,11 +4,23 @@ function AuthController($rootScope, Auth, $state) {
 
   auth.register = function() {
     Auth.register(auth.user, config).then(function(user){
+      // TODO: extract some of this code into a UserService
       $rootScope.user = user;
       alert('Thanks for signing up, ' + user.username);
       $state.go('home');
     }, function(response) {
       alert(response.data.error)
+    });
+  };
+
+  auth.login = function() {
+    Auth.login(auth.user, config).then(function(user){
+      // TODO: extract some of this code into a UserService
+      $rootScope.user = user;
+      alert("You're all signed in, " + user.username);
+      $state.go('home');
+    }, function(response) {
+      alert(response.data.error);
     });
   };
 }
