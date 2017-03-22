792b6073170234407cdc06a5be59add7e74efe13
CJ <thecjhorton@gmail.com>
2017-03-22 19:59:16 +0000

    Google Civic API is returning the correct data

--- app/controllers/google_civic_api_controller.rb
+++ app/controllers/google_civic_api_controller.rb
@@ -1,18 +1,26 @@
 class GoogleCivicApiController < ApplicationController
   def index
-    url = 'https://www.googleapis.com/civicinfo/v2/representatives?key=key&fields=divisions'
+    # setting up request and params
+    url = 'https://www.googleapis.com/civicinfo/v2/representatives?key=key&fields=offices'
     address_string = "#{api_params['street']} #{api_params['city']} #{api_params['state']} #{api_params['zip']}"
-    # address_string.gsub!(" ", "%20")
 
     @resp = Faraday.get url do |req|
       req.params['key'] = ENV['GOOGLE_API_KEY']
       req.params['address'] = address_string
     end
 
+    # getting data into usable format and return
+    # only the state and congressional district for the given address
     body_hash = JSON.parse(@resp.body)
-    byebug
-    location = body_hash['results'][0]['geometry']['location']
-    render json: location
+    result = body_hash['offices'][3]['name']
+    result.slice!("United States House of Representatives ")
+    result_array = result.split('-')
+    district = { 
+      state: result_array[0],
+      district: result_array[1]
+    }
+    
+    render json: district
   end
 
   private
