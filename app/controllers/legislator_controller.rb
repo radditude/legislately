3ad5d4c9ee6d4d92be8e19e114ef804bb2a81b40
CJ <thecjhorton@gmail.com>
2017-03-22 19:37:08 -0500

    backend routes for API calls for show & votes are working

--- app/controllers/legislator_controller.rb
+++ app/controllers/legislator_controller.rb
@@ -1,7 +1,46 @@
 class LegislatorController < ApplicationController
+
+    # TODO: there's got to be a way to have less repetitive code in these API calls. Helper?
+
     def follow
         legislator_id = params[:id]
         user_id = current_user.id
         UserLegislator.create(legislator_id: legislator_id, user_id: user_id)
+    end
+
+    def show
+      legislator_id = params[:id]
+      url = "https://api.propublica.org/congress/v1/members/#{legislator_id}.json"
+      @resp = Faraday.get url do |req|
+        req.headers['X-API-Key'] = ENV['PROPUBLICA_API_KEY']
+      end
+
+      body_hash = JSON.parse(@resp.body)
+      legislator = body_hash['results'][0]
+      render json: legislator
+    end
+
+    def votes
+      legislator_id = params[:id]
+      url = "https://api.propublica.org/congress/v1/members/#{legislator_id}/votes.json"
+      @resp = Faraday.get url do |req|
+        req.headers['X-API-Key'] = ENV['PROPUBLICA_API_KEY']
+      end
+
+      body_hash = JSON.parse(@resp.body)
+      votes = body_hash['results'][0]
+      render json: votes
+    end
+
+    def bills
+      legislator_id = params[:id]
+      url = "https://api.propublica.org/congress/v1/members/#{legislator_id}/bills/introduced.json"
+      @resp = Faraday.get url do |req|
+        req.headers['X-API-Key'] = ENV['PROPUBLICA_API_KEY']
+      end
+
+      body_hash = JSON.parse(@resp.body)
+      # bills = body_hash['results'][0]
+      render json: body_hash
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  
+
   post '/legislators/:id/follow', to: 'legislator#follow'
-  
+
+  get '/legislators/:id', to: 'legislator#show'
+
+  get '/legislators/:id/votes', to: 'legislator#votes'
+
+  get '/legislators/:id/bills', to: 'legislator#bills'
+
   post '/locateaddress', to: 'google_civic_api#index'
-  
+
   devise_for :users
-  
+
   root 'application#index'
-  
+
 end
