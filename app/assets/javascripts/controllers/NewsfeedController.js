6f001e5632508fa476355b6edc1a9e98db3c91df
CJ <thecjhorton@gmail.com>
2017-03-31 00:25:26 -0500

    solved async issues

--- app/assets/javascripts/controllers/NewsfeedController.js
+++ app/assets/javascripts/controllers/NewsfeedController.js
@@ -2,23 +2,32 @@ function NewsfeedController(UserFactory, LegislatorFactory, following, $q) {
   var feed = this;
   feed.following = following.data.following;
   feed.activity = [];
+  feed.actualData;
 
-  feed.following.forEach(function(id, index) {
-    var lastArray = [];
-    var thisArray = [];
-    var a = 0;
-    var b = 0;
-    var total;
-    LegislatorFactory.votes(id).then(function(res) {
-      // thisArray = res.data.votes;
-      // feed.activity = lastArray.concat(thisArray);
-      // lastArray = feed.activity;
-      b = index;
-      total = a + b;
-      a = total;
+  function getData() {
+    var deferred = $q.defer();
+    var callsComplete = 0;
+
+    feed.following.forEach(function(id, index) {
+      LegislatorFactory.votes(id).then(function(res) {
+        feed.activity.push(res.data.votes);
+        callsComplete += 1;
+
+        if (callsComplete === feed.following.length) {
+          deferred.resolve();
+        }
+      })
     })
-    console.log(total);
+
+    return deferred.promise;
+  }
+
+  getData().then(function(res) {
+    feed.actualData = feed.activity.reduce((a,b) => {
+      return a.concat(b);
+    });
   })
+
   // UserFactory.following().then(function(res) {
   //   feed.following = res.data.following;
   //   feed.following.forEach(function(id, index) {
