99f83cfd69148a2a61cd66f5231d02fcb7893a0d
CJ <thecjhorton@gmail.com>
2017-03-31 22:44:25 -0500

    added next election and basic stats to show page

--- app/assets/javascripts/controllers/ShowController.js
+++ app/assets/javascripts/controllers/ShowController.js
@@ -8,8 +8,18 @@ function ShowController(votes, info, following, LegislatorFactory) {
     show.following = true;
   }
 
+  if (show.profile.roles[0].senate_class === "3") {
+    show.profile.next_election = 2022;
+  } else if (show.profile.roles[0].senate_class === "2") {
+    show.profile.next_election = 2020;
+  } else if (show.profile.roles[0].senate_class === "1") {
+    show.profile.next_election = 2018;
+  } else {
+    show.profile.next_election = 2018;
+  }
+
   show.votes = votes.data.votes;
-  console.log(show.votes);
+  console.log(show.profile);
 
   show.follow = function(id, action) {
     LegislatorFactory.follow(id, action).then(function(res) {
