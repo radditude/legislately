2f81809d3958d49a983b0ccc44bd1e211da8ce75
CJ <thecjhorton@gmail.com>
2017-04-02 21:38:43 -0500

    heroku is upset about dotenv

--- app/assets/javascripts/controllers/AuthController.js
+++ app/assets/javascripts/controllers/AuthController.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict'
-  
+
   function AuthController($rootScope, Auth, $state) {
     var auth = this;
     var config = {headers: {'X-HTTP-Method-Override': 'POST'}};
@@ -23,7 +23,7 @@
         // TODO: extract some of this code into a UserService
         $rootScope.user = user;
         // TODO: make this not an alert
-        alert("You're all signed in, " + user.username);
+        // alert("You're all signed in, " + user.username);
         $state.go('newsfeed');
       }, function(response) {
         // TODO: also this
