d9c5ad273d968490c3784bf4ef2188544e091aa1
CJ <thecjhorton@gmail.com>
2017-04-06 05:52:15 +0000

    added error messages to registration email field

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,6 @@
   'use strict'
   
   angular
-    .module('legislately', ['ui.router', 'Devise', 'templates']);
+    .module('legislately', ['ui.router', 'Devise', 'templates', 'ngMessages']);
 
 }());
